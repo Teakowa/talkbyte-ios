a6f5bcb2445da322fb303b1af6f41a3569df6939
Teakowa <27560638+Teakowa@users.noreply.github.com>
2023-02-01 17:01:25 +0800

    Initial Commit

---
+++ TalkByte/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TalkByte
+//
+//  Created by Teakowa Yang on 2/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
